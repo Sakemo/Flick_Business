e232834dfd54eb72f72a5ce685e0a2c014e254e3
King <sketlen308@gmail.com>
2025-04-28 16:29:03 -0300

    Feat(Core): Implement Produto CRUD with Categoria/Fornecedor relations and tests

---
+++ sgvs-api/src/main/java/br/com/king/flick_business/FlickBusinessApplication.java
@@ -0,0 +1,13 @@
+package br.com.king.flick_business;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlickBusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlickBusinessApplication.class, args);
+	}
+
+}
---
+++ sgvs-api/src/test/java/br/com/king/flick_business/FlickBusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.king.flick_business;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlickBusinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
